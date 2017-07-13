c4b12c763711c37f3c37987a5563148b489e6d11
davidescus <rahthman_s@yahoo.com>
2017-07-13 13:51:12 +0000

    Insert fake events in app

--- files/database/seeds/EventTableSeeder.php
+++ files/database/seeds/EventTableSeeder.php
@@ -13,10 +13,43 @@ class EventTableSeeder extends Seeder {
      */
     public function run()
     {
-        User::create([
-            ‘name’ => ‘Pete Houston’,
-            ‘username’ => ‘petehouston’,
-            ‘password’ => ‘123secret’
-        ]);
+        $gmtDate = gmdate('Y-m-d H:i:s');
+
+        $tipsters = [
+            'Luca Hubber',
+            'Patrick Nickolas',
+            'Daniel Watson',
+            'David Kraig',
+        ];
+
+        $predictions = [
+            'team_1',
+            'team_2',
+            'both_to_score',
+            'over_25',
+            'over_15',
+            'under_15',
+            'under_25',
+        ];
+
+        for($x = 0; $x < 500; $x++) {
+
+            // set random event date
+            $eventDate = date('Y-m-d H:i:s', strtotime($gmtDate . '+' . rand(100, 700) . 'minute'));
+
+            Event::create([
+                'source'       => 'tipstersPortal',
+                'provider'     => $tipsters[rand(0, 3)],
+                'country'      => 'country_' . rand(0, 50),
+                'league'       => 'league_' . rand(0, 500),
+                'homeTeam'     => 'team_' . rand(0, 5000),
+                'awayTeam'     => 'team_' . rand(0, 5000),
+                'odd'          => rand(1.50, 2.50),
+                'predictionId' => $predictions[rand(0, 6)],
+                'result'       => rand(0, 5) . '-' . rand(0, 5),
+                'statusId'     => rand(1, 4),
+                'eventdate'    => $eventDate,
+            ]);
+        }
     }
 }
