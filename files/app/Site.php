48b7e4a42194b9e2b4f8448e8c11413e26a7be23
davidescus <rahthman_s@yahoo.com>
2017-07-13 13:59:09 +0000

    Add model fir Site Table

---
+++ files/app/Site.php
@@ -0,0 +1,14 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model {
+
+    protected $table = 'event';
+
+    protected $fillable = [
+        'name',
+    ];
+
+//    protected $hidden = [ ‘password’ ];
+}
