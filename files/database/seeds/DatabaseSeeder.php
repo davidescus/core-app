f9ea21d1ff97fb8d7275576aea2e4406d42ade92
davidescus <rahthman_s@yahoo.com>
2017-07-13 14:21:09 +0000

    Stop seeds for site

--- files/database/seeds/DatabaseSeeder.php
+++ files/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call('EventTableSeeder');
-         $this->call('SiteTableSeeder');
+        $this->call('EventTableSeeder');
+        //$this->call('SiteTableSeeder');
     }
 }
