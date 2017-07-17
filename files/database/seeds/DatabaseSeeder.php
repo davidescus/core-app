da689910d0e4f5d2cd516c1be551efac696230e3
davidescus <rahthman_s@yahoo.com>
2017-07-17 10:14:48 +0000

    Add prediction table seeder to db:seed

--- files/database/seeds/DatabaseSeeder.php
+++ files/database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call('EventTableSeeder');
         $this->call('SiteTableSeeder');
+        $this->call('PredictionTableSeeder');
     }
 }
