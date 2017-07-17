728587c438b183ed2d265a3135cc30ecb4a3f220
davidescus <rahthman_s@yahoo.com>
2017-07-17 09:34:34 +0000

    Create migration for prediction table

---
+++ files/database/migrations/2017_07_17_093006_create_prediction_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePredictionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prediction', function(Blueprint $table) {
+            $table->string('identifier')->primary();
+                $table->string('name')->unique();
+                $table->integer('group')->index();
+            });
+        })
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prediction');
+    }
+}
