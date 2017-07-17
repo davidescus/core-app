a9d70773941b4c7d320fca1fa9c7137e6ccc9423
davidescus <rahthman_s@yahoo.com>
2017-07-17 11:27:10 +0000

    Create package migration

---
+++ files/database/migrations/2017_07_17_111728_create_package_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePackageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('package', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('siteId');
+            $table->string('name');
+            $table->string('identifier');
+            $table->string('tipIdentifier');
+            $table->string('tableIdentifier');
+            $table->boolean('isVip');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('package');
+    }
+}
