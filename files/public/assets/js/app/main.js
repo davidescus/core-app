f01ceb42824a9d2276329e9f0e16e5da093933e4
davidescus <rahthman_s@yahoo.com>
2017-07-19 07:08:40 +0000

    Add config object in main.js

---
+++ files/public/assets/js/app/main.js
@@ -0,0 +1,14 @@
+var config = {
+    coreUrl = 'http://51.15.78.71/admin';
+};
+
+$('#left-menu li').on('click', function() {
+
+    // add class active for left mennu
+    $('#left-menu li').removeClass('active');
+    $(this).addClass('active');
+
+    // show content of desired page
+    $('.page-container').addClass('hidden');
+    $('.page-container.' + $(this).attr('target')).removeClass('hidden');
+});
