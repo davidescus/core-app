c1800d1749eb094db91dce3b2d166962abb0b69c
davidescus <rahthman_s@yahoo.com>
2017-07-19 08:29:51 +0000

    Add func to set active page on start / select page

--- files/public/assets/js/app/main.js
+++ files/public/assets/js/app/main.js
@@ -1,15 +1,24 @@
 var config = {
     coreUrl: "http://51.15.78.71/admin",
-    activePage: "dashboard"
+    activePage: "association"
 };
 
+// app start flow
+setActivePage();
+
 $('#left-menu li').on('click', function() {
+    // set active page in config
+    config.activePage = $(this).attr('target');
+    setActivePage();
+});
+
+function setActivePage() {
 
     // add class active for left mennu
     $('#left-menu li').removeClass('active');
-    $(this).addClass('active');
+    $('#left-menu li[target="' + config.activePage + '"]').addClass('active');
 
     // show content of desired page
     $('.page-container').addClass('hidden');
-    $('.page-container.' + $(this).attr('target')).removeClass('hidden');
-});
+    $('.page-container.' + config.activePage).removeClass('hidden');
+}
