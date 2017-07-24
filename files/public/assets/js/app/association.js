e488ada0f345aa435b10ed2ad24549d81c278847
davidescus <rahthman_s@yahoo.com>
2017-07-24 08:48:42 +0000

    getEventInfo will rettrive tipsters, leagues, etc

--- files/public/assets/js/app/association.js
+++ files/public/assets/js/app/association.js
@@ -1,21 +1,16 @@
 /*
 *  This method will retrive events info
-*  object args {
-*               table
-*               tipster
-*               league
-*               oddMin
-*               oddMax
-*               }
+*  object args {table}
+*  will retribe like tipsters, leagues
 */
 function getEventsInfo(args) {
 
     $.ajax({
-        url: config.coreUrl + "/event/" + args.table + "/info",
-        type: "post",
-        dataType: "json",
-        data: {},
-        beforeSend: function() {},
+        url: config.coreUrl + "/event/info?" + $.param(args),
+        type: "get",
+        //        dataType: "json",
+        //        data: {},
+        //        beforeSend: function() {},
         success: function (response) {
 
             console.log(response);
