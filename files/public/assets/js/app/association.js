871de23519a30c14a03051ab5fc4ca5fddbd14b7
davidescus <rahthman_s@yahoo.com>
2017-07-24 12:19:28 +0000

    Change template identifier from id to class

--- files/public/assets/js/app/association.js
+++ files/public/assets/js/app/association.js
@@ -16,6 +16,43 @@ function getEventsInfo(args) {
             var table = $('#table-association-' + args.table);
 
             // autocomplete provider select
+            var template = table.find('.template-select-provider').html();
+            // compile it with Template7
+            var compiledTemplate = Template7.compile(template);
+            // Now we may render our compiled template by passing required context
+            var html = compiledTemplate(response);
+            table.find('.select-provider').html(html);
+
+            // autocomplete league select
+            var template = table.find('.template-select-league').html();
+            // compile it with Template7
+            var compiledTemplate = Template7.compile(template);
+            // Now we may render our compiled template by passing required context
+            var html = compiledTemplate(response);
+            table.find('.select-league').html(html);
+
+        },
+        error: function () {}
+    });
+}
+
+/*
+* this function will retrive and show available events number
+* object args: table, provider, minOdd, maxOdd
+*/
+function getAvailableEventsNumber(args) {
+
+    $.ajax({
+        url: config.coreUrl + "/event/info?" + $.param(args),
+        type: "get",
+        //        dataType: "json",
+        //        data: {},
+        //        beforeSend: function() {},
+        success: function (response) {
+
+            var table = $('#table-association-' + args.table);
+
+            // autocomplete provider select
             var template = table.find('#template-select-provider').html();
             // compile it with Template7
             var compiledTemplate = Template7.compile(template);
@@ -34,12 +71,4 @@ function getEventsInfo(args) {
         },
         error: function () {}
     });
-}
-
-/*
-* this function will retrive and show available events number
-* object args: table, provider, minOdd, maxOdd
-*/
-function getAvailableEventsNumber(args) {
-
 }
