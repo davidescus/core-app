6b7df5f46973499f780ba67d95c399e6a063d6ca
davidescus <rahthman_s@yahoo.com>
2017-07-26 13:47:36 +0000

    Add import class to button inport events

--- files/resources/views/pages/association.php
+++ files/resources/views/pages/association.php
@@ -474,7 +474,7 @@
             </script>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary">Import Selected Events</button>
+                <button type="button" class="btn btn-primary import">Import Selected Events</button>
             </div>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
