0fafc828105c8aff8f40feeb96ca9ff9398f5837
davidescus <rahthman_s@yahoo.com>
2017-07-28 12:22:09 +0000

    Seed table

--- files/database/seeds/PackagePredictionGroupTableSeeder.php
+++ files/database/seeds/PackagePredictionGroupTableSeeder.php
@@ -14,8 +14,58 @@ class PackagePredictionGroupTableSeeder extends Seeder {
      */
     public function run()
     {
-        PackagePredictionGroup::create([
+        PackagePredictionGroup::firstOrCreate([
             'packageId'       => 1,
+            'predictionGroup' => '1x2',
+        ]);
+
+        PackagePredictionGroup::firstOrCreate([
+            'packageId'       => 2,
+            'predictionGroup' => 'o/u',
+        ]);
+
+        PackagePredictionGroup::firstOrCreate([
+            'packageId'       => 3,
+            'predictionGroup' => '1x2',
+        ]);
+
+        PackagePredictionGroup::firstOrCreate([
+            'packageId'       => 4,
+            'predictionGroup' => 'o/u',
+        ]);
+
+        PackagePredictionGroup::firstOrCreate([
+            'packageId'       => 5,
+            'predictionGroup' => '1x2',
+        ]);
+
+        PackagePredictionGroup::firstOrCreate([
+            'packageId'       => 6,
+            'predictionGroup' => 'o/u',
+        ]);
+
+        PackagePredictionGroup::firstOrCreate([
+            'packageId'       => 7,
+            'predictionGroup' => '1x2',
+        ]);
+
+        PackagePredictionGroup::firstOrCreate([
+            'packageId'       => 8,
+            'predictionGroup' => 'o/u',
+        ]);
+
+        PackagePredictionGroup::firstOrCreate([
+            'packageId'       => 9,
+            'predictionGroup' => '1x2',
+        ]);
+
+        PackagePredictionGroup::firstOrCreate([
+            'packageId'       => 10,
+            'predictionGroup' => 'o/u',
+        ]);
+
+        PackagePredictionGroup::firstOrCreate([
+            'packageId'       => 11,
             'predictionGroup' => '1x2',
         ]);
     }
