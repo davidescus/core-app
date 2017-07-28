daee188da8421e17647a98d5c7532199d702e39d
davidescus <rahthman_s@yahoo.com>
2017-07-28 12:02:39 +0000

    Add PackagePredictionGroup to seed database

--- files/database/seeds/DatabaseSeeder.php
+++ files/database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call('SiteTableSeeder');
         $this->call('PredictionTableSeeder');
         $this->call('PackageTableSeeder');
+        $this->call('PackagePredictionGroupTableSeeder');
     }
 }
