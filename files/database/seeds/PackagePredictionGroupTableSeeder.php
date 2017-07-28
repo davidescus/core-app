b915b76a422e208bd2d7f37afae21ef65905da68
davidescus <rahthman_s@yahoo.com>
2017-07-28 12:01:20 +0000

    Add db seeder for PackagePredictionGroup

---
+++ files/database/seeds/PackagePredictionGroupTableSeeder.php
@@ -0,0 +1,23 @@
+
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\PackagePredictionGroup;
+
+class PackagePredictionGroupSeeder extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+            PackagePredictionGroup::create([
+                'packageId'       => 1,
+                'predictionGroup' => '1x2',
+            ]);
+        }
+    }
+}
