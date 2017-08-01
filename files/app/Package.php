45f97f041fd945481b122739a1b900c56a0d556d
davidescus <rahthman_s@yahoo.com>
2017-08-01 08:25:26 +0000

    Add new fillable column

--- files/app/Package.php
+++ files/app/Package.php
@@ -11,7 +11,20 @@ class Package extends Model {
         'name',
         'tipIdentifier',
         'tableIdentifier',
+        'paymentName',
         'isVip',
+        'isRecurring',
+        'subscriptionType',
+        'tipsPerDay',
+        'tipsTotal',
+        'aliasTipsPerDay',
+        'aliasTipsTotal',
+        'oldPrice',
+        'discount',
+        'price',
+        'email',
+        'fromName',
+        'subject',
     ];
 
 //    protected $hidden = [ ‘password’ ];
