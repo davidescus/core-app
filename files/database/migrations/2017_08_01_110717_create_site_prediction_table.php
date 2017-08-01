c6f5e539db41b4037670a173081e61e9f5b7ebee
davidescus <rahthman_s@yahoo.com>
2017-08-01 11:10:01 +0000

    Create site_prediction migration

---
+++ files/database/migrations/2017_08_01_110717_create_site_prediction_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSitePredictionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_prediction', function(Blueprint $table) {
+            $table->string('siteId')->index();
+            $table->string('predictionIdentifier')->index();
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_prediction');
+    }
+}
