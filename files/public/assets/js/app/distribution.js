9690b76fc24eee40202761af3cafc4b538aeea78
davidescus <rahthman_s@yahoo.com>
2017-08-02 08:10:06 +0000

    Add function getDistributedEvents() to complete deistribution table with all distributed events

--- files/public/assets/js/app/distribution.js
+++ files/public/assets/js/app/distribution.js
@@ -1,3 +1,24 @@
+/*
+ * This function will complete distribution table
+ * with all distributed events
+ */
 function getDistributedEvents() {
-    alert("here will show distributed events");
+    $.ajax({
+        url: config.coreUrl + "/distribution",
+        type: "get",
+        success: function (response) {
+
+            var data = response;
+
+            console.log(data);
+
+            var element = $('#container-distributed-events');
+
+            var template = element.find('.template-table-content').html();
+            var compiledTemplate = Template7.compile(template);
+            var html = compiledTemplate(data);
+            element.find('.table-content').html(html);
+        },
+        error: function () {}
+    });
 }
