0098c4f38f6c30caa84edc7be6eedf1d3dbc5b72
davidescus <rahthman_s@yahoo.com>
2017-08-03 10:02:18 +0000

    Add management to publish events in archive

--- files/routes/web.php
+++ files/routes/web.php
@@ -455,7 +455,54 @@ $app->group(['prefix' => 'admin'], function ($app) {
     $app->post('/archive', function(Request $request) use ($app) {
         $data = $request->input('data');
 
-        return $data;
+        $alreadyPublish = 0;
+        $inserted = 0;
+
+        if (!$data)
+            return [
+                "type" => "error",
+                "message" => "No events provided!",
+            ];
+
+        foreach ($data as $value) {
+
+            $distribution = \App\Distribution::where('id', $value['distributionId'])
+                ->where('packageId', $value['packageId'])->first();
+
+            // TODO check if distributed event exists
+
+            if ($distribution->isPublish) {
+                $alreadyPublish++;
+                continue;
+            }
+
+            // set publish
+            $distribution->isPublish = 1;
+            $distribution->update();
+
+            // transform in array
+            $distribution = json_decode(json_encode($distribution), true);
+
+            // remove id and set distributionId
+            $distribution['distributionId'] = $distribution['id'];
+            unset($distribution['id']);
+
+            // TODO also send event in ArchiveHome
+
+            \App\ArchiveBig::create($distribution);
+            $inserted++;
+        }
+
+        $message = '';
+        if ($alreadyPublish)
+            $message .= "$alreadyPublish events already published to archive\r\n";
+        if ($inserted)
+            $message .= "$inserted events was published to archive\r\n";
+
+        return [
+            "type" => "success",
+            "message" => $message
+        ];
     });
 
     /*****************************************************************
