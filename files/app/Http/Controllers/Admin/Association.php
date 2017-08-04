939a2bfb797193dd804b8b29070c367c5923fcd1
davidescus <rahthman_s@yahoo.com>
2017-08-04 15:09:41 +0000

    Create Admin\Association controller

---
+++ files/app/Http/Controllers/Admin/Association.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Association extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+    }
+
+    public function get() {}
+
+    public function store() {}
+
+    public function update() {}
+
+    public function destroy() {}
+}
