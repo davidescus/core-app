5384949a621ef93e40d33b2cba19352c389ee4d6
davidescus <rahthman_s@yahoo.com>
2017-08-08 18:46:22 +0000

    Get associated predictions for each package

--- files/app/Http/Controllers/Admin/Package.php
+++ files/app/Http/Controllers/Admin/Package.php
@@ -40,7 +40,39 @@ class Package extends Controller
      */
     public function getPackagesBySite($siteId)
     {
-        return \App\Package::where('siteId', $siteId)->get();
+        $packages = \App\Package::where('siteId', $siteId)->get();
+
+        $predictions = \App\Prediction::all();
+
+        //iterate al packages and get predictions
+        foreach ($packages as $k => $package) {
+
+            // get allowed predictions
+            $allowedPredictions = \App\PackagePrediction::where('packageId', $package['id'])->get();
+
+            $pred = $predictions;
+            foreach ($pred as $kp => $p) {
+
+                // make default association false
+                $pred[$kp]['isAssociated'] = false;
+                foreach ($allowedPredictions as $kap => $ap) {
+
+                    // if association exists make it true
+                    if ($p->identifier === $ap->predictionIdentifier)
+                        $pred[$kp]['isAssociated'] = true;
+                }
+            }
+
+            // create new array and sort predicitons
+            $data = [];
+            foreach ($pred as $p) {
+               $data[$p->group][] = $p;
+            }
+
+            $packages[$k]['predictions'] = $data;
+        }
+
+        return $packages;
     }
 
 }
