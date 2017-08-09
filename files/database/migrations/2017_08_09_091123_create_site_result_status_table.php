f71da16f3a06b8c2095fce1b53e9c64ac0b9411b
davidescus <rahthman_s@yahoo.com>
2017-08-09 17:46:23 +0000

    Add autoincrement id

--- files/database/migrations/2017_08_09_091123_create_site_result_status_table.php
+++ files/database/migrations/2017_08_09_091123_create_site_result_status_table.php
@@ -14,6 +14,7 @@ class CreateSiteResultStatusTable extends Migration
     public function up()
     {
         Schema::create('site_result_status', function(Blueprint $table) {
+            $table->increments('id');
             $table->integer('siteId')->index();
             $table->integer('statusId')->index();
             $table->string('statusName');
