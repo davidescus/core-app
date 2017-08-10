2e2cc58c112ec574bfa3a52be0d6e42a95df567f
davidescus <rahthman_s@yahoo.com>
2017-08-10 18:31:04 +0000

    Add autoicrement column id

--- files/database/migrations/2017_08_01_115454_create_site_package_table.php
+++ files/database/migrations/2017_08_01_115454_create_site_package_table.php
@@ -14,6 +14,7 @@ class CreateSitePackageTable extends Migration
     public function up()
     {
         Schema::create('site_package', function(Blueprint $table) {
+            $table->increments('id');
             $table->integer('siteId')->index();
             $table->string('packageId')->index();
         });
