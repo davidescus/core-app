eb24dd25a77feef060caf3ce05c834bf4e6692da
davidescus <rahthman_s@yahoo.com>
2017-08-15 10:08:18 +0000

    Make code primary key.

--- files/database/migrations/2017_08_15_093433_create_country_table.php
+++ files/database/migrations/2017_08_15_093433_create_country_table.php
@@ -14,9 +14,10 @@ class CreateCountryTable extends Migration
     public function up()
     {
         Schema::create('country', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('code')->index();
+            $table->string('code');
             $table->string('name');
+
+            $table->primary('code');
         });
     }
 
