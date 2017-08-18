2b68cc2fbb22b1c1e10eba2ba3e7bf52144e40e3
davidescus <rahthman_s@yahoo.com>
2017-08-18 13:38:08 +0000

    Add controller for distribution.

---
+++ files/app/Http/Controllers/Admin/Distribution.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Distribution extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /*
+     * @string $date format: Y-m-d || 0
+     * @return array()
+     */
+    public function index($date = null)
+    {
+        $data = [];
+
+        // default set current date GMT
+        if ($date === null || $date == 0)
+            $date = gmdate('Y-m-d');
+
+        foreach (\App\Site::all() as $site) {
+            // set siteName
+            $data[$site->id]['name'] = $site->name;
+            $data[$site->id]['id'] = $site->id;
+
+            // get associated packages frm site_package
+            $assocPacks = \App\SitePackage::select('packageId')->where('siteId', $site->id)->get()->toArray();
+            foreach ($assocPacks as $assocPack) {
+                // get package
+                $package = \App\Package::find($assocPack['packageId']);
+
+                // get events for package
+                $distributedEvents = \App\Distribution::where('packageId', $package->id)->where('systemdate', $date)->get();
+
+                $data[$site->id]['packages'][$assocPack['packageId']]['id'] = $package->id;
+                $data[$site->id]['packages'][$assocPack['packageId']]['name'] = $package->name;
+                $data[$site->id]['packages'][$assocPack['packageId']]['tipsPerDay'] = $package->tipsPerDay;
+                $data[$site->id]['packages'][$assocPack['packageId']]['eventsNumber'] = count($distributedEvents);
+                $data[$site->id]['packages'][$assocPack['packageId']]['events'] = $distributedEvents;
+            }
+        }
+
+        return $data;
+    }
+
+    public function get() {}
+
+    public function store() {}
+
+    public function update() {}
+
+    public function destroy() {
+
+    }
+}
