eb43c4022afbec01039fc1b8db893198d02db45e
davidescus <rahthman_s@yahoo.com>
2017-08-22 11:39:00 +0000

    Add controllers for archive and match

---
+++ files/app/Http/Controllers/Admin/Match.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Match extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /*
+     * @return array()
+     */
+    public function index()
+    {
+    }
+
+    public function get() {}
+
+    public function store() {}
+
+    public function update() {}
+
+    public function destroy() {}
+
+}
