cf56744050e2c885a5038d4c2f551ef9494a0914
davidescus <rahthman_s@yahoo.com>
2017-09-05 11:27:30 +0000

    Move func in controller.

--- files/routes/web.php
+++ files/routes/web.php
@@ -180,45 +180,9 @@ $app->group(['prefix' => 'client'], function ($app) {
     });
 
     // @param integer $id
-    // get archive-big for site.
+    // get archive-big events for site.
     // @return array() indexed by table idintifier.
-    $app->get('/update-archive-big/{id}', function ($id) use ($app) {
-
-        $site = \App\Site::find($id);
-        if (!$site)
-            return false;
-
-        // results name and class
-        $results = [];
-        foreach (\App\SiteResultStatus::where('siteId', $id)->get()->toArray() as $k => $v)
-           $results[$v['statusId']] = $v;
-
-        // prediction
-        $predictions = [];
-        foreach (\App\SitePrediction::where('siteId', $id)->get()->toArray() as $k => $v)
-           $predictions[$v['predictionIdentifier']] = $v;
-
-        $events = \App\ArchiveBig::where('siteId', $id)
-            ->where('isPublishInSite', '1')->get()->toArray();
-
-        $data = [];
-        foreach ($events as $e) {
-
-            // add result statusName and statusClass
-            $e['statusName'] = $results[$e['statusId']]['statusName'];
-            $e['statusClass'] = $results[$e['statusId']]['statusClass'];
-
-            //predictionName
-            $e['predictionName'] = $predictions[$e['predictionId']]['name'];
-
-            $table = $e['tableIdentifier'];
-            $year  = date('Y', strtotime($e['systemDate']));
-            $month = date('m', strtotime($e['systemDate']));
-            $data[$table][$year][$month][] = $e;
-        }
-
-        return $data;
-    });
+    $app->get('/update-archive-big/{id}', 'Client\ArchiveBig@index');
 });
 
     /* -------------------------------------------------------------------
