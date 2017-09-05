e204e9564dc06fb087186334572c6b55cfdf6add
davidescus <rahthman_s@yahoo.com>
2017-09-05 11:53:38 +0000

    Create Admin\Login and move func from routes.php.

---
+++ files/app/Http/Controllers/Admin/Login.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Login extends Controller
+{
+
+    // each login will generate a new token
+    // @param string $email
+    // @param string $password
+    // @return array()
+    public function index(Request $r)
+    {
+        $email = $r->input('email');
+        $password = $r->input('password');
+
+        $user = \App\User::where('email', $email)
+            ->where('password', sha1($password))->first();
+
+        if (!$user)
+            return [
+                'success' => 0,
+            ];
+
+        $token = md5(microtime());
+
+        $user->token = $token;
+        $user->save();
+
+        return [
+            'success' =>1,
+            'token'   => $token,
+        ];
+    }
+
+}
