9664da8ef2a83cdf16d3bacf0e831eb2acfd11f4
davidescus <rahthman_s@yahoo.com>
2017-09-05 10:55:40 +0000

    Add new classes.

---
+++ files/app/Http/Controllers/Client/ArchiveBig.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ArchiveBig extends Controller
+{
+
+    public function index()
+    {
+    }
+
+    public function get() {}
+
+    public function store() {}
+
+    public function update() {}
+
+    public function destroy() {}
+
+}
