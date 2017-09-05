0374f2265daa34599495b280ec4e75034427dc05
davidescus <rahthman_s@yahoo.com>
2017-09-05 11:30:41 +0000

    Add Client\Configuration class.

---
+++ files/app/Http/Controllers/Client/Configuration.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Configuration extends Controller
+{
+
+    public function index()
+    {
+    }
+
+    public function get() {}
+
+    public function store() {}
+
+    public function update() {}
+
+    public function destroy() {}
+
+}
