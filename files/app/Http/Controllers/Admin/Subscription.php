d8f47e777d12f1f79792ce83eb3f25f158a18038
davidescus <rahthman_s@yahoo.com>
2017-09-12 09:12:25 +0000

    Add func to create new subscription.

--- files/app/Http/Controllers/Admin/Subscription.php
+++ files/app/Http/Controllers/Admin/Subscription.php
@@ -25,7 +25,63 @@ class Subscription extends Controller
     //  - compare values with original package.
     // @return array()
     public function store(Request $r) {
+        $packageId = $r->input('packageId');
+        $name = $r->input('name');
+        $subscription = $r->input('subscription');
+        $price = $r->input('price');
+        $type = $r->input('type');
+        $dateStart = $r->input('dateStart');
+        $dateEnd = $r->input('dateEnd');
+        $customerEmail = $r->input('customerEmail');
 
+        // check if package exist
+        $package = \App\Package::find($packageId);
+        if (!$packageId)
+            return [
+                'type' => 'error',
+                'mesasge' => 'Package not exist anymore'
+            ];
+
+        // get siteId
+        $sitePackage = \App\SitePackage::where('packageId', $packageId)->first();
+        $siteId = $sitePackage->siteId;
+
+        // get customer
+        $customer = \App\Customer::where('email', $customerEmail)->first();
+
+        $data = [
+            'customerId' => $customer->id,
+            'siteId' => $siteId,
+            'packageId' => $package->id,
+            'type' => $type,
+            'subscription' => $subscription,
+            'dateStart' => $dateStart,
+            'dateEnd' => $dateEnd,
+            'status' => 'active',
+        ];
+
+        if ($type === 'tips') {
+            $data['tipsLeft'] = $subscription;
+            unset($data['dateStart']);
+            unset($data['dateEnd']);
+        }
+
+        // if user already have active subscription on same package
+        // set status waiting
+        if (\App\Subscription::where('customerId', $customer->id)
+            ->where('packageId', $packageId)
+            ->where('status', 'active')->count())
+        {
+            $data['status'] = 'waiting';
+        }
+
+        // create subscription
+        $subscription = \App\Subscription::create($data);
+        return [
+            'type' => 'success',
+            'message' => 'Subscription was created with success!',
+            'data' => $subscription,
+        ];
     }
 
     public function update() {}
