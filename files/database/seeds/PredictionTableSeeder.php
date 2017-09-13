c3bc1c4017b66e00e1c1c72d9686e59034819d1a
davidescus <rahthman_s@yahoo.com>
2017-09-13 15:40:45 +0000

    Add all predictiions.

--- files/database/seeds/PredictionTableSeeder.php
+++ files/database/seeds/PredictionTableSeeder.php
@@ -13,31 +13,88 @@ class PredictionTableSeeder extends Seeder {
      */
     public function run()
     {
-        $predictions = [
-            '1x2:team_1:Team 1',
-            '1x2:team_2:Team 2',
-            '1x2:equal:Equal',
-            '1x2:team_1_ht:Team 1 HT',
-            '1x2:team_2_ht:Team 2 HT',
-            '1x2:equal_ht:Equal HT',
-            'o/u:over_15:Over 1.5',
-            'o/u:over_25:Over 25',
-            'o/u:over_35:Over 3.5',
-            'o/u:under_15:Under 1.5',
-            'o/u:under_25:Under 25',
-            'o/u:under_35:Under 3.5',
+
+        $groups = [
+            '1x2' => [
+                ['identifier' => 'team1', 'name' => 'Team 1'],
+                ['identifier' => 'team2', 'name' => 'Team 2'],
+                ['identifier' => 'equal', 'name' => 'Equal'],
+            ],
+            'g/g' => [
+                ['identifier' => 'bothToScore', 'name' => 'Both To Score'],
+                ['identifier' => 'oneToScore', 'name' => 'One To Score'],
+                ['identifier' => 'noGoal', 'name' => 'No Goal'],
+            ],
+            'o/u' => [
+                ['identifier' => 'over_1.25', 'name' => 'Over 1.25'],
+                ['identifier' => 'under_1.25', 'name' => 'Under 1.25'],
+                ['identifier' => 'over_1.5', 'name' => 'Over 1.5'],
+                ['identifier' => 'under_1.5', 'name' => 'Under 1.5'],
+                ['identifier' => 'over_1.75', 'name' => 'Over 1.75'],
+                ['identifier' => 'under_1.75', 'name' => 'Under 1.75'],
+                ['identifier' => 'over_2', 'name' => 'Over 2'],
+                ['identifier' => 'under_2', 'name' => 'Under 2'],
+                ['identifier' => 'over_2.25', 'name' => 'Over 2.25'],
+                ['identifier' => 'under_2.25', 'name' => 'Under 2.25'],
+                ['identifier' => 'over_2.5', 'name' => 'Over 2.5'],
+                ['identifier' => 'under_2.5', 'name' => 'Under 2.5'],
+                ['identifier' => 'over_2.75', 'name' => 'Over 2.75'],
+                ['identifier' => 'under_2.75', 'name' => 'Under 2.75'],
+                ['identifier' => 'over_3', 'name' => 'Over 3'],
+                ['identifier' => 'under_3', 'name' => 'Under 3'],
+                ['identifier' => 'over_3.25', 'name' => 'Over 3.25'],
+                ['identifier' => 'under_3.25', 'name' => 'Under 3.25'],
+                ['identifier' => 'over_3.5', 'name' => 'Over 3.5'],
+                ['identifier' => 'under_3.5', 'name' => 'Under 3.5'],
+                ['identifier' => 'over_3.75', 'name' => 'Over 3.75'],
+                ['identifier' => 'under_3.75', 'name' => 'Under 3.75'],
+            ],
+            'ah' => [
+                ['identifier' => 'team1-ah_0', 'name' => 'Team 1 0 AH'],
+                ['identifier' => 'team2-ah_0', 'name' => 'Team 2 0 AH'],
+                ['identifier' => 'team1-ah_+0.5', 'name' => 'Team 1 +0.5 AH'],
+                ['identifier' => 'team2-ah_+0.5', 'name' => 'Team 2 +0.5 AH'],
+                ['identifier' => 'team1-ah_-0.5', 'name' => 'Team 1 -0.5 AH'],
+                ['identifier' => 'team2-ah_-0.5', 'name' => 'Team 2 -0.5 AH'],
+                ['identifier' => 'team1-ah_+1', 'name' => 'Team 1 +1 AH'],
+                ['identifier' => 'team2-ah_+1', 'name' => 'Team 2 +1 AH'],
+                ['identifier' => 'team1-ah_-1', 'name' => 'Team 1 -1 AH'],
+                ['identifier' => 'team2-ah_-1', 'name' => 'Team 2 -1 AH'],
+                ['identifier' => 'team1-ah_+1.25', 'name' => 'Team 1 +1.25 AH'],
+                ['identifier' => 'team2-ah_+1.25', 'name' => 'Team 2 +1.25 AH'],
+                ['identifier' => 'team1-ah_-1.25', 'name' => 'Team 1 -1.25 AH'],
+                ['identifier' => 'team2-ah_-1.25', 'name' => 'Team 2 -1.25 AH'],
+                ['identifier' => 'team1-ah_+1.5', 'name' => 'Team 1 +1.5 AH'],
+                ['identifier' => 'team2-ah_+1.5', 'name' => 'Team 2 +1.5 AH'],
+                ['identifier' => 'team1-ah_-1.5', 'name' => 'Team 1 -1.5 AH'],
+                ['identifier' => 'team2-ah_-1.5', 'name' => 'Team 2 -1.5 AH'],
+                ['identifier' => 'team1-ah_+1.75', 'name' => 'Team 1 +1.75 AH'],
+                ['identifier' => 'team2-ah_+1.75', 'name' => 'Team 2 +1.75 AH'],
+                ['identifier' => 'team1-ah_-1.75', 'name' => 'Team 1 -1.75 AH'],
+                ['identifier' => 'team2-ah_-1.75', 'name' => 'Team 2 -1.75 AH'],
+                ['identifier' => 'team1-ah_+2', 'name' => 'Team 1 +2 AH'],
+                ['identifier' => 'team2-ah_+2', 'name' => 'Team 2 +2 AH'],
+                ['identifier' => 'team1-ah_-2', 'name' => 'Team 1 -2 AH'],
+                ['identifier' => 'team2-ah_-2', 'name' => 'Team 2 -2 AH'],
+                ['identifier' => 'team1-ah_+2.25', 'name' => 'Team 1 +2.25 AH'],
+                ['identifier' => 'team2-ah_+2.25', 'name' => 'Team 2 +2.25 AH'],
+                ['identifier' => 'team1-ah_-2.25', 'name' => 'Team 1 -2.25 AH'],
+                ['identifier' => 'team2-ah_-2.25', 'name' => 'Team 2 -2.25 AH'],
+                ['identifier' => 'team1-ah_+2.5', 'name' => 'Team 1 +2.5 AH'],
+                ['identifier' => 'team2-ah_+2.5', 'name' => 'Team 2 +2.5 AH'],
+                ['identifier' => 'team1-ah_-2.5', 'name' => 'Team 1 -2.5 AH'],
+                ['identifier' => 'team2-ah_-2.5', 'name' => 'Team 2 -2.5 AH'],
+            ],
         ];
 
-        foreach ($predictions as $prediction) {
-            $group = explode(':', $prediction)[0];
-            $identifier = explode(':', $prediction)[1];
-            $name = explode(':', $prediction)[2];
-
-            Prediction::firstOrCreate([
-                'identifier' => $identifier,
-                'name'       => $name,
-                'group'      => $group
-            ]);
+        foreach ($groups as $group => $value) {
+            foreach ($value as $v) {
+                Prediction::firstOrCreate([
+                    'identifier' => $v['identifier'],
+                    'name'       => $v['name'],
+                    'group'      => $group,
+                ]);
+            }
         }
     }
 }
