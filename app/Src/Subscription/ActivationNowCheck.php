038ef5ba027427632c4cc1a2b6c458291ac136c1
davidescus <rahthman_s@yahoo.com>
2017-10-17 11:42:27 +0000

    Add tests for ActivationNowCheck class

--- app/Src/Subscription/ActivationNowCheck.php
+++ app/Src/Subscription/ActivationNowCheck.php
@@ -5,12 +5,27 @@ namespace App\Src\Subscription;
 class ActivationNowCheck
 {
     private $events;
-    public isValid;
+    public $isValid;
 
     public function __construct($events)
     {
         $this->events = $events;
+    }
 
-        var_dump($this->isValid);
+    public function validateEvents()
+    {
+        if (!$this->events) {
+            $this->isValid = true;
+            return;
+        }
+
+        foreach ($this->events as $event) {
+            if ($event->isPublish == 1) {
+                $this->isValid = false;
+                return;
+            }
+        }
+
+        $this->isValid = true;
     }
 }
--- tests/app/Src/Subscription/ActivationNowCheckTest.php
+++ tests/app/Src/Subscription/ActivationNowCheckTest.php
@@ -8,9 +8,58 @@ class ActivationNowCheckTest extends TestCase
     public function testShouldFailIFNotInstanceOf()
     {
         $this->assertInstanceOf(
-            \App\Src\Subscription\ActivationCheck::class,
-            new \App\Src\Subscription\ActivationCheck()
+            \App\Src\Subscription\ActivationNowCheck::class,
+            new \App\Src\Subscription\ActivationNowCheck([])
         );
+    }
+
+    public function testIsValidShouldBeTrueIfThereInNoEvents()
+    {
+        $activation = new \App\Src\Subscription\ActivationNowCheck([]);
+        $activation->validateEvents();
+        $this->assertTrue($activation->isValid);
+    }
+
+    public function testIsValidShouldBeFalseIfOneEventIsPublish()
+    {
+        $one = new \stdClass();
+        $one->isPublish = '1';
+        $two = new \stdClass();
+        $two->isPublish = '0';
+        $activation = new \App\Src\Subscription\ActivationNowCheck([
+            0 => $one,
+            1 => $two
+        ]);
+        $activation->validateEvents();
+        $this->assertFalse($activation->isValid);
+    }
+
+    public function testIsValidShouldBeFalseIfMoreEventsIsPublish()
+    {
+        $one = new \stdClass();
+        $one->isPublish = '1';
+        $two = new \stdClass();
+        $two->isPublish = '1';
+        $activation = new \App\Src\Subscription\ActivationNowCheck([
+            0 => $one,
+            1 => $two
+        ]);
+        $activation->validateEvents();
+        $this->assertFalse($activation->isValid);
+    }
+
+    public function testIsValidShouldBeTrueIfNoEventsArePublished()
+    {
+        $one = new \stdClass();
+        $one->isPublish = '0';
+        $two = new \stdClass();
+        $two->isPublish = '0';
+        $activation = new \App\Src\Subscription\ActivationNowCheck([
+            0 => $one,
+            1 => $two
+        ]);
+        $activation->validateEvents();
+        $this->assertTrue($activation->isValid);
     }
 }
 
