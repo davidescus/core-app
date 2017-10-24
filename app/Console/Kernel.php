97ac7a1ef3f41493f6217b87be5d0b7f7a22aec6
davidescus <rahthman_s@yahoo.com>
2017-10-24 13:06:56 +0000

    Add schedule to take new events from tipstersportal at every five minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,25 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+       //$schedule->call(function() {
+       //    $admin = new \App\User();
+       //    $admin->name = "test";
+       //    $admin->email = gmdate('Y-m-d H:i:s') . rand(0, 1000000);
+       //    $admin->password = 'test';
+       //    $admin->save();
+       //})->everyMinute();
+
+        $schedule->call(function() {
+            $admin = new \App\User();
+            $admin->name = "test";
+            $admin->email = gmdate('Y-m-d H:i:s') . rand(0, 1000000);
+            $admin->password = 'test';
+            $admin->save();
+        })->timezone('GMT')->dailyAt('23:59');
+
+        // get new events from portal at every 5 minutes.
+        $schedule->call(function() {
+            new \App\Http\Controllers\Cron\PortalNewEvents();
+        })->everyFiveMinutes();
     }
 }
