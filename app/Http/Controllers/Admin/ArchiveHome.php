b3aa2957141b29483eb60d042f9eaa82889fd195
davidescus <rahthman_s@yahoo.com>
2017-11-02 09:40:42 +0000

    Add controller for archive home.

---
+++ app/Http/Controllers/Admin/ArchiveHome.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class ArchiveHome extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /*
+     * @return array()
+     */
+    public function index()
+    {
+    }
+
+    // @param integer $id
+    // @return event || null
+    public function get($id) {
+        return \App\ArchiveHome::find($id);
+    }
+
+    // @param $id,
+    // toogle show/hide an event from archiveHome
+    // @return array()
+    public  function toogleShowHide($id)
+    {
+        $event = \App\ArchiveHome::find($id);
+
+        if (!$event)
+            return [
+                'type' => 'error',
+                'message' => "This event not exist anymore!",
+            ];
+
+        $event->isVisible ? $event->isVisible = '0' : $event->isVisible = '1';
+        $event->save();
+
+        return [
+            'type' => 'success',
+            'message' =>"Status of event was successfful changed!",
+        ];
+    }
+
+    public function store() {}
+
+    public function update() {}
+
+    public function destroy() {}
+
+}
+
