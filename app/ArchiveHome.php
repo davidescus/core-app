58c03e4379539451524596c61e2b3e412de68c5d
davidescus <rahthman_s@yahoo.com>
2017-11-02 09:33:56 +0000

    Create Archive Home model.

---
+++ app/ArchiveHome.php
@@ -0,0 +1,44 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ArchiveHome extends Model {
+
+    protected $table = 'archive_home';
+
+    protected $fillable = [
+        'distributionId',
+        'associationId',
+        'eventId',
+        'siteId',
+        'packageId',
+        'source',
+        'provider',
+        'tableIdentifier',
+        'tipIdentifier',
+        'isPublish',
+        'isPublishInSite',
+        'isVisible',
+        'isNoTip',
+        'isVip',
+        'country',
+        'countryCode',
+        'league',
+        'leagueId',
+        'homeTeam',
+        'homeTeamId',
+        'awayTeam',
+        'awayTeamId',
+        'odd',
+        'predictionId',
+        'predictionName',
+        'result',
+        'statusId',
+        'eventDate',
+        'mailingDate',
+        'systemDate',
+    ];
+
+//    protected $hidden = [ ‘password’ ];
+}
+
