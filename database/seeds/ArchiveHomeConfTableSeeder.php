b0f028a68d5843ca76c546ebcda159d1e34fd2f6
davidescus <rahthman_s@yahoo.com>
2017-11-03 11:23:04 +0000

    Seed archive home config.

--- database/seeds/ArchiveHomeConfTableSeeder.php
+++ database/seeds/ArchiveHomeConfTableSeeder.php
@@ -13,6 +13,21 @@ class ArchiveHomeConfTableSeeder extends Seeder {
      */
     public function run()
     {
+        $packages = \App\Package::all();
+        foreach($packages as $p) {
+            $confExists = ArchiveHomeConf::where('siteId', $p->siteId)
+                ->where('tableIdentifier', $p->tableIdentifier)
+                ->count();
+
+            if (! $confExists) {
+                ArchiveHomeConf::create([
+                    'siteId'          => $p->siteId,
+                    'tableIdentifier' => $p->tableIdentifier,
+                    'eventsNumber'    => 100,
+                    'dateStart'       => '2017-01-01',
+                ]);
+            }
+        }
     }
 }
 
