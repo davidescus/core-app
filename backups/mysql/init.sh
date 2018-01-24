ea6ab49129fd6eea92f5393df4b759a3a4e1a813
davidescus <rahthman_s@yahoo.com>
2018-01-24 10:15:08 +0000

    Change to absolute path.

--- backups/mysql/init.sh
+++ backups/mysql/init.sh
@@ -3,22 +3,27 @@
 # gdrive configuration
 #http://olivermarshall.net/how-to-upload-a-file-to-google-drive-from-the-command-line/
 
+# dir where script is
+DIR="$(cd "$(dirname "$0")" && pwd -P)"
+
 DATE=`date '+%Y-%m-%d-%H-%M-%S'`
-STORAGE=$PWD/storage
+STORAGE=$DIR/storage
 FILE=$STORAGE/$DATE.sql
 TARGZ=$FILE.tar.gz
 
+#echo $DIR This is theddddddddddddddddddddddddddddddddddddddddddddd
+
 # source .env file
-source ../../.env;
+source $DIR/../../.env;
 
 # create file
 docker exec $DB_CONTAINER /usr/bin/mysqldump -u $DB_ROOT_USER --password=$DB_ROOT_PASS $DB_DATABASE > $FILE
 
-tar -cvzf storage/$DATE.sql.tar.gz storage/$DATE.sql
+tar -cvzf $STORAGE/$DATE.sql.tar.gz $STORAGE/$DATE.sql
 
 rm -f $FILE
 
-gdrive upload $TARGZ --parent $GDRIVE_DIR_BACKUP_ID
+/usr/local/bin/gdrive upload $TARGZ --parent $GDRIVE_DIR_BACKUP_ID
 
 # delete files
 for i in `ls -pt $STORAGE/*.sql.tar.gz | tail -n+100`;
