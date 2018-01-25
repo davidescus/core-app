f8989774ee9a374a9ce90efc7b7d2ba43d0e8a73
davidescus <rahthman_s@yahoo.com>
2018-01-25 14:38:51 +0000

    Add cron to publish archives in sites.

--- app/Console/Commands/PublishArchives.php
+++ app/Console/Commands/PublishArchives.php
@@ -11,9 +11,30 @@ class PublishArchives extends CronCommand
 
         $info = [
             'published' => 0,
+            'notPublished' => 0,
         ];
 
-        print_r($info);
+        $maxDate = gmdate('Y-m-d H:i:s', time() - 60 * 60);
+        $archives = \App\ArchivePublishStatus::where('created_at', '>', $maxDate)
+            ->limit(10)->get();
+
+        $actionInstance = new \App\Http\Controllers\Admin\Client\TriggerAction();
+
+        foreach ($archives as $archive) {
+
+            if ($archive->type == 'archiveHome')
+                $wasPublish = $actionInstance->updateArchiveHome($archive->siteId);
+            else
+                $wasPublish = $actionInstance->updateArchiveBig($archive->siteId);
+
+            if ($wasPublish['type'] == 'success') {
+                $info['published']++;
+                continue;
+            }
+
+            $info['notPublished']++;
+            $info['errors'][] = $wasPublish['message'];
+        }
 
         $this->info(json_encode($info));
         $this->stopCron($cron, $info);
