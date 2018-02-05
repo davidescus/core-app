0ed8f2bcb8a706cb018310b8d0eafc50b0e825c0
davidescus <rahthman_s@yahoo.com>
2018-02-05 14:17:00 +0000

    Rename file.

--- database/migrations/2018_02_05_140351_create_auto_units_daily_schedule_table.php
+++ database/migrations/2018_02_05_140351_create_auto_units_daily_schedule_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAutoUnitsDailyScheduleTable.php extends Migration
+class CreateAutoUnitsDailyScheduleTable extends Migration
 {
     /**
      * Run the migrations.
